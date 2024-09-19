c9511186fd238bf7c7eb058bcca4aead32f62042
Yan-7 <yanivalmagor7@gmail.com>
2024-09-19 12:18:35 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express =  require("express");
+const app = express();
+
+// app.use((req,res) => {
+//     console.log('new request');
+//     res.send('<h1>This is my webpage!<h1>')
+    
+// })
+
+app.get('/cats', (req, res) => {
+    res.send('MAOOOO')
+    
+})
+app.post('/cats',(req,res) => {
+    res.send('maoooooooooooooo posted')
+})
+
+app.get('/dogs', (req,res) => {
+    res.send('WOOF')
+})
+
+app.get('/search',(req,res) => {
+    console.log(res.query);
+    res.send('this is a search')
+    
+})
+
+app.listen(3000, () => {
+    console.log('listening on 3000');
+})
+
